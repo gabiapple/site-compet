7def3d4b248038c2f5b1710fdba7397be38224bf
Saulo Antunes <sauloantuness@gmail.com>
2016-02-03 07:44:32 -0200

    update

--- code.py
+++ code.py
@@ -1,20 +1,33 @@
 import web
         
 urls = (
-    '/*', 'index',
-    '/favicon.ico', 'icon'
+    '/', 'index',
+    '/favicon.ico', 'icon',
+    '/([a-zA-Z-]*)', 'pages'
 )
+
 app = web.application(urls, globals())
 
-class index:        
-    def GET(self):
-    	f = open('static/index.html','r+')
-    	page = f.read()
-    	f.close()
-    	return page
+class index:
+	def GET(self):
+		p = pages()
+		return p.GET('../index')
+
+class pages:
+	def GET(self, url):
+		try:
+			f = open('static/pages/' + url + '.html', 'r')
+			post = f.read()
+			f.close()
+
+			render = web.template.frender('static/template.html')
+			return render(post)
+		except:
+			raise web.seeother('/')
 
 class icon:
-    def GET(self): raise web.seeother("/static/favicon.ico")
+    def GET(self):
+    	raise web.seeother("/static/favicon.ico")
 
 if __name__ == "__main__":
     app.run()
